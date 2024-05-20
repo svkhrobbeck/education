5cd97b65a3413c5ca1b40b8715bb41b409725bd9
svkhrobbeck <svkhrobbeck@gmail.com>
2024-05-21 02:23:26 +0500

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "@/styles/globals.css";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="home" />
+      </Head>
+    </>
+  );
+};
+
+export default HomePage;
