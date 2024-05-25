a35ffb8e88379eea5148b876f8d77ff6a338ca77
svkhrobbeck <svkhrobbeck@gmail.com>
2024-05-25 19:53:14 +0500

    style: :rocket: add inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
 import "@/assets/styles/main.scss";
 
+
+const inter = Inter({
+  weight: ["300", "400", "500", "600", "700", "900"],
+  style: ["normal"],
+  subsets: ["latin", "cyrillic"],
+  display: "swap",
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
