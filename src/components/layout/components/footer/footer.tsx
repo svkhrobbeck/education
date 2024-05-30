248970d22a2598afa718eec3c89e81be2a06af1f
svkhrobbeck <svkhrobbeck@gmail.com>
2024-05-30 17:40:20 +0500

    feat(components): :recycle: complete footer component

--- src/components/layout/components/footer/footer.tsx
+++ src/components/layout/components/footer/footer.tsx
@@ -1,12 +1,29 @@
 import { FC } from "react";
 import cx from "classnames";
+import Link from "next/link";
+import { format } from "date-fns";
 
 import { FooterProps } from "./footer.props";
+import { Container } from "@/components";
 
 import cls from "./footer.module.scss";
 
 const Footer: FC<FooterProps> = ({ className, ...props }) => {
-  return <footer className={cx(cls.footer, className)} {...props}></footer>;
+  const date = format(new Date(), "yyyy");
+
+  return (
+    <footer className={cx(cls.footer, className)} {...props}>
+      <Container className={cls.footerContainer}>
+        <p className={cls.footerCopyright}>Nextech © {date}. All Rights Reserved</p>
+        <Link className={cls.footerLink} href="/">
+          Terms of use
+        </Link>
+        <Link className={cls.footerLink} href="/">
+          Privacy Policy
+        </Link>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
