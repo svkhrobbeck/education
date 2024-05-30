0e5cd3c6ebaf82351e34938e1b3780593e522365
svkhrobbeck <svkhrobbeck@gmail.com>
2024-05-30 23:54:31 +0500

    feat(layouts): :boom: add context to main layout

--- src/layouts/main/main.tsx
+++ src/layouts/main/main.tsx
@@ -1,12 +1,16 @@
 import { FunctionComponent } from "react";
 
+import { IContext } from "@/context/props";
+import ContextProvider from "@/context";
 import { Layout } from "@/components";
 
-const Main = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+const Main = <T extends Record<string, unknown> & IContext>(Component: FunctionComponent<T>) => {
   return function LayoutWithProps(props: T) {
     return (
       <Layout>
-        <Component {...props} />
+        <ContextProvider menu={props.menu} category={props.category}>
+          <Component {...props} />
+        </ContextProvider>
       </Layout>
     );
   };
