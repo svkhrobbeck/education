50e1b180e50dc857406f47e7263be9c87717b0d0
svkhrobbeck <svkhrobbeck@gmail.com>
2024-05-31 14:42:59 +0500

    feat(components): :art: add books and courses icons

---
+++ src/components/icon/list/courses.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Courses: FC = () => {
+  return (
+    <svg width="24" height="22" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path d="M24 15.405c0-.915-.59-1.688-1.406-1.98V6.143L24 5.562 12 .747 0 5.562l12 4.815 9.188-3.67v6.719a2.106 2.106 0 0 0-1.407 1.98c0 .838.495 1.557 1.205 1.897l-1.168 3.505 1.333.445.74-2.218.74 2.218 1.333-.445-1.169-3.505A2.106 2.106 0 0 0 24 15.405Z" />
+    </svg>
+  );
+};
+
+export default Courses;
