c47dd167c174cbdf24b960671f41bf2911a03efb
svkhrobbeck <svkhrobbeck@gmail.com>
2024-05-31 14:43:41 +0500

    feat(context): :sparkles: add category prop

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,19 +1,25 @@
-import { createContext, PropsWithChildren, useContext as useCtx, useState } from "react";
+import { createContext, PropsWithChildren, useContext as useCtx, useEffect, useState } from "react";
 
 import * as Types from "@/helpers/interfaces";
 import { IContext } from "./props";
 
 const initialValue = {
   menu: [],
+  category: 0,
   setMenu: () => {},
 };
 
 const Context = createContext<IContext>(initialValue);
 
-const ContextProvider = ({ children, menu: initialMenu }: PropsWithChildren<Omit<IContext, "setMenu">>) => {
-  const [menu, setMenu] = useState<Types.Menu.Item[]>(initialMenu);
+const ContextProvider = ({ children, category, ...props }: PropsWithChildren<Omit<IContext, "setMenu">>) => {
+  const [menu, setMenu] = useState<Types.Menu.Item[]>(props.menu);
 
-  return <Context.Provider value={{ menu, setMenu }}>{children}</Context.Provider>;
+  useEffect(() => {
+    setMenu(props?.menu);
+    // eslint-disable-next-line
+  }, []);
+
+  return <Context.Provider value={{ menu, setMenu, category }}>{children}</Context.Provider>;
 };
 
 export const useContext = () => useCtx(Context);
--- src/context/props.ts
+++ src/context/props.ts
@@ -2,5 +2,6 @@ import * as Types from "@/helpers/interfaces";
 
 export interface IContext {
   menu: Types.Menu.Item[];
+  category: number;
   setMenu?: (menu: Types.Menu.Item[]) => void;
 }
