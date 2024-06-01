8d1b69801df9fd74963b9e6d6f849ccd2488eecd
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-01 12:56:53 +0500

    fix(components): :lipstick: update divider style

--- src/components/divider/divider.tsx
+++ src/components/divider/divider.tsx
@@ -6,9 +6,7 @@ import { DividerProps } from "./divider.props";
 import cls from "./divider.module.scss";
 
 const Divider: FC<DividerProps> = ({ className, ...props }) => {
-  return (
-    <hr className={cx(cls.divider, className)} {...props} />
-  );
+  return <hr className={cx(cls.divider, className)} {...props} />;
 };
 
 export default Divider;
