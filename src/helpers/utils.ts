c5deb4e32f24373ab0234cfe50589b23d094b714
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-01 15:24:34 +0500

    feat(utils): :sparkles: create utils

---
+++ src/helpers/utils.ts
@@ -0,0 +1,10 @@
+export const convertToUSD = (price: number): string => {
+  return price
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+    .concat(" $");
+};
+
+export const dedectedReview = (number: number): string => {
+  return number > 1 ? "Reviews" : "Review";
+};
