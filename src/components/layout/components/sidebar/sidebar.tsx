0e57a19ed197cdbdde96056f534fb34f4d5ed5e5
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-01 15:24:02 +0500

    feat(components): :sparkles: add logo and divider to sidebar

--- src/components/layout/components/sidebar/sidebar.tsx
+++ src/components/layout/components/sidebar/sidebar.tsx
@@ -2,11 +2,19 @@ import { FC } from "react";
 import cx from "classnames";
 
 import { SidebarProps } from "./sidebar.props";
+import { Divider, Logo } from "@/components";
+import { Menu } from "..";
 
 import cls from "./sidebar.module.scss";
 
 const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
-  return <aside className={cx(cls.sidebar, className)} {...props}></aside>;
+  return (
+    <aside className={cx(cls.sidebar, className)} {...props}>
+      <Logo />
+      <Divider />
+      <Menu />
+    </aside>
+  );
 };
 
 export default Sidebar;
