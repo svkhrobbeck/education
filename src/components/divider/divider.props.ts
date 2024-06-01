44f533657a32ab5b2dea089eb3a4d21d0399a685
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-01 16:04:46 +0500

    feat(components): :recycle: add new props to divider component

--- src/components/divider/divider.props.ts
+++ src/components/divider/divider.props.ts
@@ -1,3 +1,5 @@
 import { ComponentProps } from "react";
 
-export type DividerProps = ComponentProps<"hr">;
+export type DividerProps = ComponentProps<"hr"> & {
+  direction?: "horizontal" | "vertical";
+};
--- src/components/divider/divider.tsx
+++ src/components/divider/divider.tsx
@@ -5,8 +5,8 @@ import { DividerProps } from "./divider.props";
 
 import cls from "./divider.module.scss";
 
-const Divider: FC<DividerProps> = ({ className, ...props }) => {
-  return <hr className={cx(cls.divider, className)} {...props} />;
+const Divider: FC<DividerProps> = ({ className, direction = "horizontal", ...props }) => {
+  return <hr className={cx(cls.divider, cls[`divider--${direction}`], className)} {...props} />;
 };
 
 export default Divider;
