f31eac9d6fd54d9ac30b3e4aaed572c2c3b82e20
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-02 22:24:01 +0500

    feat(components): :sparkles: add search and sort icons into icon component

---
+++ src/components/icon/list/sort.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Sort: FC = () => {
+  return (
+    <svg width="20" height="13" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="20" height="3" rx="1.5" fill="currentColor" />
+      <rect y="5" width="14" height="3" rx="1.5" fill="currentColor" />
+      <rect y="10" width="8" height="3" rx="1.5" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default Sort;
