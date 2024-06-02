c12d2f9b35fac7bbab1a0103b3b0fba286508775
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-02 14:55:35 +0500

    fix: :bug: fix mini issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["media.graphassets.com"],
+    dangerouslyAllowSVG: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
