3847b909fb2a25e0e7b6a762dc24c10ceb9e4669
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-02 19:49:43 +0500

    feat(components): :sparkles: add close and user icons to icon component

--- src/components/icon/list/index.ts
+++ src/components/icon/list/index.ts
@@ -1,6 +1,8 @@
 export { default as Rate } from "./rate";
 export { default as Star } from "./star";
+export { default as User } from "./user";
 export { default as Books } from "./books";
 export { default as Arrow } from "./arrow";
 export { default as Check } from "./check";
+export { default as Close } from "./close";
 export { default as Courses } from "./courses";
