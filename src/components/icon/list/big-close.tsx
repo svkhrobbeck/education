544e8faf619e8b0015b3d0224393f468e0f3a10d
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-03 12:55:29 +0500

    feat(components): :sparkles: add big-close and menu icons into icon component

---
+++ src/components/icon/list/big-close.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const BigClose: FC = () => {
+  return (
+    <svg width="19" height="19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect y="16.51" width="23" height="3" rx="1.5" transform="rotate(-45 0 16.51)" fill="currentColor" />
+      <rect x="2.253" width="23" height="3" rx="1.5" transform="rotate(45 2.253 0)" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default BigClose;
---
+++ src/components/icon/list/menu.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Menu: FC = () => {
+  return (
+    <svg width="20" height="17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="20" height="3" rx="1.5" fill="currentColor" />
+      <rect y="7" width="20" height="3" rx="1.5" fill="currentColor" />
+      <rect y="14" width="20" height="3" rx="1.5" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default Menu;
