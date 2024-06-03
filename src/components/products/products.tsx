44f2834206f723bf4361f3cd144b6e669891e193
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-03 10:41:30 +0500

    feat(components): :dizzy: integrate framer-motion animations to products component

--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import cx from "classnames";
+import { AnimatePresence } from "framer-motion";
 
 import { ProductsProps } from "./products.props";
 import { Product } from "..";
@@ -7,24 +8,27 @@ import { Product } from "..";
 import cls from "./products.module.scss";
 
 const Products: FC<ProductsProps> = ({ className, products, ...props }) => {
-  // const spring = {
-  //   type: "spring",
-  //   stiffness: 500,
-  //   damping: 10,
-  // };
+  const spring = {
+    type: "spring",
+    stiffness: 500,
+    damping: 10,
+  };
 
-  // const animations = {
-  //   initial: { scale: 0 },
-  //   animate: { scale: 1 },
-  //   exit: { scale: 1 },
-  // };
+  const animations = {
+    initial: { scale: 0 },
+    animate: { scale: 1 },
+    exit: { scale: 1 },
+  };
 
   return (
     <section className={cx(cls.products, className)} {...props}>
-      {/* <AnimatePresence>
-        {state.products.map((c, idx) => <Product layout transition={spring} {...animations} product={c} />)}
-      </AnimatePresence> */}
-      {!!products.length && products.map((product, idx) => <Product key={idx} product={product} />)}
+      {!!products.length && (
+        <AnimatePresence mode="wait">
+          {products.map((product, idx) => (
+            <Product key={idx} transition={spring} {...animations} layout product={product} />
+          ))}
+        </AnimatePresence>
+      )}
     </section>
   );
 };
