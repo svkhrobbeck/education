c5ef8139b8eb1f931f6f2775faf208a86efc99ef
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-03 11:45:09 +0500

    feat(components): :sparkles: create scroll-up component

--- src/components/layout/components/index.ts
+++ src/components/layout/components/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from "./header";
 export { default as Footer } from "./footer";
 export { default as Search } from "./search";
 export { default as Sidebar } from "./sidebar";
+export { default as ScrollUp } from "./scroll-up";
---
+++ src/components/layout/components/scroll-up/scroll-up.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import { motion } from "framer-motion";
+
+import { useScrollY } from "@/hooks";
+import { Icon } from "@/components";
+
+import cls from "./scroll-up.module.scss";
+
+const ScrollUp: FC = () => {
+  const scrollY = useScrollY();
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <>
+      {scrollY > 150 && (
+        <motion.button
+          className={cls.scrollUp}
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { duration: 0.6 } }}
+          exit={{ y: 100, opacity: 0, transition: { duration: 0.6 } }}
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.2 },
+          }}
+          onClick={scrollToTop}
+        >
+          <Icon {...{ name: "Up", width: 21, height: 13, color: "#fff" }} />
+        </motion.button>
+      )}
+    </>
+  );
+};
+
+export default ScrollUp;
