394881980f1e002f19130fe7a3d3be0feb801ba4
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-03 13:07:16 +0500

    feat(components): :art: make header and sidebar components responsive

--- src/components/layout/components/header/header.tsx
+++ src/components/layout/components/header/header.tsx
@@ -1,15 +1,64 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { motion } from "framer-motion";
 import cx from "classnames";
 
 import { HeaderProps } from "./header.props";
-import { Container } from "@/components";
+
+import { Container, IconButton } from "@/components";
+import { Sidebar, Logo } from "..";
 
 import cls from "./header.module.scss";
 
 const Header: FC<HeaderProps> = ({ className, ...props }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => setIsOpen(prev => !prev);
+
+  const variants = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        stiffnes: 20,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: "100%",
+    },
+  };
+
   return (
     <header className={cx(cls.header, className)} {...props}>
-      <Container className={cls.headerContainer}>Header</Container>
+      <Container className={cls.headerContainer}>
+        <Logo />
+        {isOpen ? (
+          <IconButton
+            {...{
+              className: cls.closeIcon,
+              name: "BigClose",
+              width: 19,
+              height: 19,
+              color: "#140342",
+              appearance: "white",
+            }}
+            onClick={toggleMenu}
+          />
+        ) : (
+          <IconButton
+            {...{ name: "Menu", width: 20, height: 17, color: "#140342", appearance: "white" }}
+            onClick={toggleMenu}
+          />
+        )}
+        <motion.div
+          variants={variants}
+          initial="hidden"
+          animate={isOpen ? "visible" : "hidden"}
+          className={cls.mobileMenu}
+        >
+          <Sidebar />
+        </motion.div>
+      </Container>
     </header>
   );
 };
