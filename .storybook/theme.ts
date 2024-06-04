b6946ae2434a04210b3055cd5d0a8466b8c63aba
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-04 12:20:32 +0500

    feat(storybook): :sparkles: integrate storybook initial

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Nextech",
+  brandUrl: "https://nextech.soatov.uz",
+  brandImage: "/light-logo.svg",
+  brandTarget: "_blank",
+});
