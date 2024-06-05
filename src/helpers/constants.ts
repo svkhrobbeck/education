f1ec9e5d5dcd0c6be7d71f4e2b3a1a1f076f4e24
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-05 15:03:49 +0500

    feat(helpers): add company list constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -10,3 +10,36 @@ export const levelMenu: Types.Menu.Level[] = [
   { route: "books", name: "Books", iconName: "Books", id: Enums.Categories.Books },
   { route: "courses", name: "Courses", iconName: "Courses", id: Enums.Categories.Courses },
 ];
+
+export const company: Types.ICompany[] = [
+  {
+    name: "Amazon",
+    width: 84,
+    height: 26,
+  },
+  {
+    name: "Amd",
+    width: 83,
+    height: 20,
+  },
+  {
+    name: "Cisco",
+    width: 74,
+    height: 42,
+  },
+  {
+    name: "Dropcam",
+    width: 122,
+    height: 24,
+  },
+  {
+    name: "Logitech",
+    width: 82,
+    height: 25,
+  },
+  {
+    name: "Spotify",
+    width: 99,
+    height: 30,
+  },
+];
