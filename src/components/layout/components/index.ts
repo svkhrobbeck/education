1adf84e2f8c3bb82313d1f47d942583c86250b0d
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-05 16:26:35 +0500

    refactor(components): :recycle: move scroll-up component into components folder

--- src/components/layout/components/index.ts
+++ src/components/layout/components/index.ts
@@ -2,4 +2,3 @@ export { default as Menu } from "./menu";
 export { default as Header } from "./header";
 export { default as Footer } from "./footer";
 export { default as Sidebar } from "./sidebar";
-export { default as ScrollUp } from "./scroll-up";
