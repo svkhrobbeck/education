33fe83d947113dd03e6b833d19dc388716404ac6
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-05 16:25:21 +0500

    feat(assets): :sparkles: add new icons and images into assets folder

---
+++ src/assets/icons/nextjs.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+
+const NextJs: FC = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M9 15V9l7.745 10.65A9 9 0 1 1 19 17.657M15 12V9" />
+    </svg>
+  );
+};
+
+export default NextJs;
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeBg } from "./bg.png";
+export { default as HeroImg } from "./hero.png";
 export { default as LogoDark } from "./logo.svg";
 export { default as LogoLight } from "./logo-light.svg";
